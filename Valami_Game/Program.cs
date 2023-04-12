9ed5e7057f30903fc92d3d2ccf47efde65c0194b
RafaelMark12 <rafael.mark1224@gmail.com>
2023-04-12 20:04:49 +0200

    Történet rövid folytatása, helyette kisebb leírásokat tettem az adott konkrét dologhoz a főprogramban.

--- Valami_Game/Program.cs
+++ Valami_Game/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using Valami_Game;
+using System.Media;
 
 namespace Valami_Game
 {
@@ -16,26 +18,38 @@ namespace Valami_Game
             Terulet valami = new ElsoTerem();
             Npc ember = new Rasszista();
 
+
+
+
+
+
+
+            /*Történet kezdete*/
+
             string ora = "Fizika";
             Animalt("Üdvözöllek a Neumann János Technikumjában.");
-            Thread.Sleep(2000);
+            Thread.Sleep(1);
             Animalt("Kérlek nyomj egy Entert!");
             Console.ReadLine();
             Console.Clear();
+            Thread.Sleep(1000);
             Animalt("Szeretne-e bejönni az iskolába? (Y/N)");
             string input = Console.ReadLine();
             if (input == "Y")
             {
                 Console.Clear();
                 Animalt("Indulás...");
-                Thread.Sleep(5000);
-                Console.ReadLine();
+                Thread.Sleep(2000);
+                
             }
             else if (input == "N")
             {
                 Console.Clear();
                 Animalt("A játék hamarosan leáll...");
                 Thread.Sleep(2000);
+                return; //arra szolgál a return itt, hogyha a játék le akarna állítani, 
+                        //ezzel a paranccsal tudjuk kiléptetni a console-ból a személyt.
+                
             }
             else
             {
@@ -43,6 +57,7 @@ namespace Valami_Game
                 Animalt("ÉRVÉNYTELEN A BEMENET!");
                 Animalt("A játék hamarosan leáll...");
                 Thread.Sleep(2000);
+                return;
             }
             Console.Clear();
             Animalt("Beléptél az Aulába. \n\nHamarosan indul az óra, ideje bemenni az órádra.");
@@ -67,31 +82,15 @@ namespace Valami_Game
                 Animalt("Büfé");
                 Animalt("Terem");
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
+
+
+
+
+        /*A szöveg animálása ezzel a függvénnyel írható ki*/
         static void Animalt(string text)
         {
+            
             foreach (char c in text)
             {
                 Console.Write(c);
